62aadd8670ae8ce02e457d7c101baa83a97b21d4
Ayumi <79387837+Ayumidev@users.noreply.github.com>
2023-09-25 23:07:31 -0300

    tcc

---
+++ src/Publico-Alvo/Publico.jsx
@@ -0,0 +1,67 @@
+import './App.css'
+
+import Logo from './Img/Logo.png'
+import Exemplo from './Img/img-exe.png'
+import LMenu from './Img/list.png'
+
+function App(){
+    return(
+        <div className='publico'>
+        <header className='Menupb'>
+            <div className='logopb'>
+              <img src={Logo} alt="Logo do Smart FireMan"/>
+            </div>
+            <div className='menupb'>
+              <img src={LMenu} alt="Logo do Smart FireMan"/>
+            </div>
+            <div className='titulopb'>
+              <h1>Público Alvo</h1>
+            </div>
+        </header>
+
+        <main className='contentpb'>
+            <div className='textpb'>
+                <ul>
+                    <li>Famílias preocupadas com a segurança: </li>
+                    <p>Proteja sua família com o Smart FireMan - o robô de <br />
+                    detecção de incêndio que oferece tranquilidade e <br />
+                    segurança em casa. Detecção avançada de incêndio em <br />
+                    um dispositivo compacto e fácil de instalar.</p>
+
+                    <li>Escolas e instituições educacionais: </li>
+                    <p>Priorize a segurança dos alunos e colaboradores com o <br />
+                    Smart FireMan. Tecnologia inteligente de detecção de <br />
+                    incêndio em um design compacto, adaptável a qualquer <br />
+                    ambiente educacional.</p>
+
+                    <li>Empresas e escritórios: </li>
+                    <p>Mantenha sua equipe e negócio protegidos com o Smart <br />
+                    FireMan - o robô de detecção de incêndio que oferece <br />
+                    uma resposta rápida e eficaz em situações de <br />
+                    emergência. Segurança garantida em um dispositivo <br />
+                    compacto e de fácil integração.</p>
+
+                    <li>Construtoras e empreendimentos imobiliários: </li>
+                    <p>Destaque-se no mercado imobiliário oferecendo o <br />
+                    Smart FireMan - o diferencial de segurança essencial <br />
+                    para residências e edifícios. Integre facilmente a <br />
+                    tecnologia avançada de detecção de incêndio em seus <br />
+                    projetos.</p>
+
+                    <li>Engenheiros e profissionais de segurança: </li>
+                    <p>Confie na eficiência do Smart FireMan para aprimorar a <br />
+                    segurança em seus projetos. Detecção abrangente de <br />
+                    gases, calor, fumaça e chamas em um dispositivo <br />
+                    compacto, atendendo às exigências de prevenção de <br />
+                    incêndios.</p>
+                </ul>
+            </div>
+            <div className='figurepb'>
+              <img src={Exemplo} alt="Logo do Smart FireMan"/>
+            </div>
+        </main>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Sobre/Sobre.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+
+import Logo from './Img/Logo.png'
+import Exemplo from './Img/Imagem-exemplo.png'
+import LMenu from './Img/list.png'
+
+
+
+function App() {
+    return (
+      <div className='sobre'>
+      <header className='Menus'>
+        <div className='logos'>
+          <img src={Logo} alt="Logo do Smart FireMan"/>
+        </div>
+        <div className='titulos'>
+          <h1>Sobre</h1>
+        </div>
+        <div className='menus'>
+          <img src={LMenu} alt="Logo do Smart FireMan"/>
+        </div>
+      </header>
+
+      <main className='contents'>
+        <div className='texts'>
+          <p>O projeto consiste em uma simples caixa, que haverá um sensor instalado, <br /> 
+          este sensor é programado para detectar gás, fumaça ou até mesmo fogo,<br /> 
+          sendo assim eficiente na área da cozinha, onde se ocorre maior parte dos <br /> 
+          incidentes envolvendo fogo. Junto ao sensor haverá um buzzer que fará <br /> 
+          um barulho para chamar a atenção das pessoas, gerando logo uma atenção <br /> 
+          a mais, e se certificar de que não está em perigo, dando melhor sensação <br /> 
+          de segurança para as pessoas. Além de conter um sensor que assim que algo <br /> 
+          for detectado irá alertar ao usuário e o serviço de emergência</p>
+        </div>
+        <div className='figures'>
+          <img src={Exemplo} alt="Logo do Smart FireMan"/>
+        </div>
+      </main>
+      </div>
+    )
+  }
+  
+  export default App
